7e915789f7f54c647269fcd9770ceaf9617fcafd
ThyTechGuy <danielbankole33@yahoo.com>
2022-12-12 12:17:06 +0000

    ERC1155 token tested

--- contracts/METASCHOOL/tokenStandards.sol
+++ contracts/METASCHOOL/tokenStandards.sol
@@ -5,10 +5,21 @@ pragma solidity ^0.8.13;
 /*importing openZeppelin library, 
 Ownable ERC721 - includes basic definition for a contract*/
 
-import "https://github.com/OpenZepplin/openzepplin-contracts/contracts/token/ERC721/ERC721.sol";
+/*
+ERC-1155 is a brand-new token standard that strives to combine the finest features 
+of prior token standards to build a fungibility-agnostic, 
+gas-efficient token contract. This standard eliminates the drawbacks and 
+limitations of its predecessors and builds upon their success. 
+The distinctive feature that makes it stand out is the fact that it uses a 
+single smart contract to represent multiple types of tokens (both fungible and non-fungible) at once.
+*/
+
+// import "https://github.com/OpenZepplin/openzepplin-contracts/contracts/token/ERC721/ERC721.sol";
+import "erc721a/contracts/ERC721A.sol";
 
 //define name your contract, symbol(in the ERC721 constructor then inherit ERC721,
-contract Hellonft is ERC721("HelloNft", "HNFT") {
+// contract Hellonft is ERC721("HelloNft", "HNFT") {
+contract Hellonft is ERC721A("HelloNft", "HNFT") {
     
     uint tokenId; //tokenId is initialized to zero
     
