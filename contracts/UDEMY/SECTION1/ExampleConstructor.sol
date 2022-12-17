d8de35a166d0f0b64b3cca9dac2489921a4abe38
ThyTechGuy <danielbankole33@yahoo.com>
2022-12-17 19:16:15 +0000

    comment  added for good information

---
+++ contracts/UDEMY/SECTION1/ExampleConstructor.sol
@@ -0,0 +1,31 @@
+//SPDX-License-Identifier: MIT
+
+
+
+pragma solidity 0.8.15;
+
+/*The constructor is a special function. 
+*It is automatically called during Smart Contract deployment. 
+And it can never be called again after that.
+*It also has a special name! It's simply called constructor() { ... }..*/
+
+contract ExampleConstructor {
+
+    address public myAddress;
+
+    //function starts at the initialization of program
+    constructor(address _someAddress) {
+        myAddress = _someAddress;
+    }
+
+    function setMyAddress(address _myAddress) public {
+        myAddress = _myAddress;
+    }
+
+    function setMyAddressToMsgSender() public {
+            myAddress = msg.sender;
+
+    }
+}
+
+
