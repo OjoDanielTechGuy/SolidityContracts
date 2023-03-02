5bbad211d4e8e52efcc93e3be3788a25ef438555
ThyTechGuy <danielbankole33@yahoo.com>
2023-03-02 10:59:59 +0100

    intro to web3.js

--- contracts/UDEMY/PROJECT-ERC20-TOKEN/scripts/accounts.js
+++ contracts/UDEMY/PROJECT-ERC20-TOKEN/scripts/accounts.js
@@ -1,9 +1,16 @@
-/*This is to be on remix for web using the environment (Injector Provider) 
-to get the right information*/
+/*Web3.js is a javascript library that lets you interact with a blockchain node via its RPC interface.
+That can be a RESTful HTTP interface, or WebSockets.
+
+- This is to be on remix for web using the environment (Injector Provider) to get the 
+right information.
+
+web3.eth === runs an anonymous main function, which is using the async/await syntac to 
+work with promises**/
 
 (async () => {
     let accounts = await web3.eth.getAccounts(); //this gets the accounts details
     console.log(accounts, accounts.length);//.length gets the number of accounts
+    console.log("Accounts 1: ", accounts[0]); //This gets the first account
 
     let balance = await web3.eth.getBalance(accounts[0]); //this gets the accounts balance
     console.log(balance);
