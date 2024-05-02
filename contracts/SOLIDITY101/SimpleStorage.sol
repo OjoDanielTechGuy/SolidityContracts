0facc02e1b173ab657c6d598c256b3c70063d811
DANIEL OJO <danielbankole33@yahoo.com>
2024-05-02 13:01:13 +0100

    Update SimpleStorage.sol

--- contracts/SOLIDITY101/SimpleStorage.sol
+++ contracts/SOLIDITY101/SimpleStorage.sol
@@ -1,1 +1,35 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.22;
+
+contract SimpleStorage {
+    
+    //favouriteNumber initializes to 0
+    uint favouriteNumber;
+
+    struct Person {
+        uint favouriteNumber;
+        string name;
+    }
+
+    //dynamic array
+    Person[] public listOfPeople;
+
+    //mapping
+    mapping(string => uint) public nameToFavouriteNumber;
+
+    function store(uint _favouriteNumber) public {
+        favouriteNumber = _favouriteNumber;
+    }
+
+    //view, pure
+    function retrieve() public view returns(uint) {
+        return favouriteNumber;
+    }
+
+    //adding people via array
+    function addPerson(string memory _name, uint _favouriteNumber) public {
+        listOfPeople.push(Person(_favouriteNumber, _name));
+        nameToFavouriteNumber[_name] = _favouriteNumber; 
+    }
+}
